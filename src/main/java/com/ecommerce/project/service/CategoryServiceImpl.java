2584631148721c97f3eb2160fa705e3ab1b9d4ec
aartiPurohit <rti.raj.15nov2002@gmail.com>
2025-10-01 17:18:51 +0530

    implemented-jpa-repositories

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryReopsitory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,36 +14,42 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private long nextId = 1l;
-    private List<Category> categories = new ArrayList<>();
+//    private long nextId = 1l;
+//    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryReopsitory categoryReopsitory;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryReopsitory.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId ++);
-        categories.add(category);
+//        category.setCategoryId(nextId ++);
+        categoryReopsitory.save(category);
     }
 
     @Override
     public String deleteCategory(long categoryId) {
+        List <Category> categories = categoryReopsitory.findAll();
+
         Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
-        categories.remove(category);
+        categoryReopsitory.delete(category);
         return "Category with categoryId: " + categoryId + " deleted successfully...";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List <Category> categories = categoryReopsitory.findAll();
         // can do it in the same way as the deleteCategory().
         Optional <Category> categoryOptional = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
         if(categoryOptional.isPresent()) {
             Category existingCategory = categoryOptional.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            Category saveCategory = categoryReopsitory.save(existingCategory);
+            return saveCategory;
         }
         else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found!");
