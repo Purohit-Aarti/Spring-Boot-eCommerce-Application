7f1c5301f24f6b0aea3e1b452bf43d0ac1e2ee2c
aartiPurohit <rti.raj.15nov2002@gmail.com>
2025-10-03 21:56:29 +0530

    implemented more custom exceptions

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,15 +1,12 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.exception.MyGlobalExceptionHandler;
+import com.ecommerce.project.exception.APIException;
 import com.ecommerce.project.exception.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryReopsitory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,12 +20,20 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryReopsitory.findAll();
+        List <Category> allCategories = categoryReopsitory.findAll();
+        if(allCategories.isEmpty()) {
+            throw new APIException("No categories created till now");
+        }
+        return allCategories;
     }
 
     @Override
     public void createCategory(Category category) {
 //        category.setCategoryId(nextId ++);
+        Category savedCategory = categoryReopsitory.findByCategoryName(category.getCategoryName());
+        if(savedCategory != null) {
+            throw new APIException("Category with the name " + category.getCategoryName() + " exists");
+        }
         categoryReopsitory.save(category);
     }
 
